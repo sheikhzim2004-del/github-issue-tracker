857830bc55d2d2bd21bece5f97cd53c5d8215a13
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-03-08 16:44:34 +0600

    mic title add by this modal

--- scripts/home.js
+++ scripts/home.js
@@ -5,7 +5,14 @@ const allFilterBtn = document.getElementById('all-filter-btn');
 const openFilterBtn = document.getElementById('open-filter-btn');
 const closedFilterBtn = document.getElementById('closed-filter-btn');
 const countIssues = document.getElementById('issues-count');
-const boxDetails =document.getElementById('details-container');
+const boxDetails = document.getElementById('details-container');
+
+// sound the the word
+function pronounceWord(title) {
+    const utterance = new SpeechSynthesisUtterance(title);
+    utterance.lang = "en-EN"; // English
+    window.speechSynthesis.speak(utterance);
+}
 
 // show loading spinner
 const showSpinner = () => {
@@ -25,14 +32,14 @@ const loadIssues = async () => {
     hideSpinner()
     displayIssues(data.data);
     alldata = data.data; //alldata er modde shokol issues card add kora
-    console.log(alldata)
+    // console.log(alldata)
 }
 
 // fetch id wise issue
-const showModal = async (id)=> {
+const showModal = async (id) => {
     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
     const res = await fetch(url);
-    const details =await res.json();
+    const details = await res.json();
     displayIssuesDetails(details.data)
 }
 
@@ -52,7 +59,11 @@ const showModal = async (id)=> {
 // }
 
 // show modal
-const displayIssuesDetails = (issue)=>{
+const displayIssuesDetails = (issue) => {
+
+    const borderClass = issue.status.trim().toLowerCase() === 'open' ? 'border-green-500' : 'border-purple-500';
+    boxDetails.classList.add('border-4', borderClass);
+
     boxDetails.innerHTML = `
     <h1 class="font-bold text-2xl">${issue.title}</h1>
             <div class="gap-2 flex">
@@ -62,9 +73,9 @@ const displayIssuesDetails = (issue)=>{
             </div>
             <div class="flex gap-1">
                 <div class="badge badge-soft badge-secondary border-1 border-red-300 p-3 rounded-full font-medium"><i
-                        class="fa-solid fa-bug"></i>${issue.labels[0]? issue.labels[0] : 'No labels'}</div>
+                        class="fa-solid fa-bug"></i>${issue.labels[0] ? issue.labels[0] : 'No labels'}</div>
                 <div class="badge badge-soft badge-warning border-1 border-yellow-300 p-3 rounded-full font-medium"><i
-                        class="fa-regular fa-life-ring"></i>${issue.labels[1]? issue.labels[1] : 'No labels'}</div>
+                        class="fa-regular fa-life-ring"></i>${issue.labels[1] ? issue.labels[1] : 'No labels'}</div>
             </div>
             <p class="py-4">${issue.description}</p>
             <div class="flex bg-gray-100 p-5 rounded-md">
@@ -77,13 +88,18 @@ const displayIssuesDetails = (issue)=>{
                     <p class="badge text-white rounded-full ${issue.priority === 'high' ? 'badge-secondary' : issue.priority === 'medium' ? 'badge-warning' : 'badge-info'}">${issue.priority}</p>
                 </div>
             </div>
-            <div class="modal-action">
-                <form method="dialog">
+            <div class="modal-action flex justify-between">
+            <button  id="mic" class="btn btn-accent"><i class="fa-solid fa-microphone"></i></button>
+                <form method="dialog" flex justify-center>
                     <!-- if there is a button in form, it will close the modal -->
                     <button class="btn btn-error">Close</button>
                 </form>
             </div>
     `
+    const mic = document.getElementById('mic');
+    mic.addEventListener('click',()=>{
+        pronounceWord(issue.title)
+    })
     document.getElementById('issue_modal').showModal()
 }
 
@@ -124,7 +140,7 @@ const displayIssues = (issues) => {
                     <div class="badge badge-soft badge-warning border-1 border-yellow-300 p-3 rounded-full font-medium"><i class="fa-regular fa-life-ring"></i>${issue.labels[1] ? issue.labels[1] : 'No labels'}</div>
                 </div>
                 <hr class="border-0 h-0.5 bg-gray-300">
-                <p class="text-[#64748B]">${issue.author}</p>
+                <p class="text-[#64748B]">#${issue.id} ${issue.author}</p>
                 <p class="text-[#64748B]">${new Date(issue.updatedAt).toLocaleDateString()}</p>
             </div>
         </div>
